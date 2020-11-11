a1525af59dc0d2083ccda190c88293269c56c631
FrancescoDale <fd060862@gmail.com>
2020-11-11 10:49:52 +0100

    array con i to-dos

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,14 @@
 var app = new Vue ( {
+    el : '#root',
+    data : {
+        todos : [
+            'uova',
+            'farina',
+            'latte',
+            'biscotti',
+        ]
 
+    },
 
 
 

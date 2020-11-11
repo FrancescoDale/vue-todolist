26cdf10db7edc93fe4add580cdf3002ba448db2b
FrancescoDale <fd060862@gmail.com>
2020-11-11 17:23:18 +0100

    modificata la funzione enterKey

--- assets/main.js
+++ assets/main.js
@@ -29,7 +29,7 @@ var app = new Vue ( {
         //creo una funzione che prende come parametro il tasto premuto
         enterKey () {
             // al verificarsi della condizione inserisce newTodo nell'array todos
-            this.todos.push(this.newTodo);
+            this.addtodo();
         },
     },
 
